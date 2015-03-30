6ae7eb8bfc31864570b91662949949a7c776a0ce
Marcel Müller <neikos@neikos.email>
2015-03-31 01:32:49 +0200

    Refactored music.hs

--- music.hs
+++ music.hs
@@ -1,27 +1,20 @@
-
-data Pitch = Pitch Float deriving (Show, Read, Ord, Eq)
 data Note = C | C' | D | D' | E | F | F' | G | G'| A | A' | B
             deriving (Show, Enum, Read, Bounded, Ord, Eq)
 data Step = Base | Half | Whole deriving (Show, Enum, Read, Bounded, Ord, Eq)
 
 type Scale = [Note]
 
-
-nextNote :: Note -> Note
-nextNote B = C
-nextNote n = succ n
-
 bound :: Int -> Int
 bound n = n `mod` 12
 
 toBoundEnum = toEnum.bound
 
-adjustNote :: Note -> Step -> Note
-adjustNote n s = toBoundEnum $ fromEnum n + fromEnum s
+adjustNoteByStep :: Note -> Step -> Note
+adjustNoteByStep n s = toBoundEnum $ fromEnum n + fromEnum s
 
 createScale :: [Step] -> Note -> Scale
 createScale [] n = [n]
-createScale (x:xs) n = [n] ++ createScale xs (adjustNote n x)
+createScale (step:steps) n = [n] ++ createScale steps (adjustNoteByStep n step)
 
 majorScale = createScale [Whole, Whole, Half, Whole, Whole, Whole]
 minorScale = createScale [Whole, Half, Whole, Whole, Half, Whole]
