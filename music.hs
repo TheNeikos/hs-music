dde35eeca9cf53baeba2467e0470e24a6cabf3bc
Marcel Müller <neikos@neikos.email>
2015-03-31 02:59:16 +0200

    Switched to fully featured notes (with octaves)

--- music.hs
+++ music.hs
@@ -5,7 +5,16 @@ data Step = Base | Half | Whole deriving (Show, Enum, Read, Bounded, Ord, Eq)
 data Interval = P1 | Mi2 | M2 | Mi3 | M3 | P4 | P5 | Mi6 | M6 | Mi7 | M7 | P8
             deriving (Show, Enum, Read, Bounded, Ord, Eq)
 
-type Scale = [Note]
+data Beat = WholeNote | HalfNote | Quarter | Eighth | Sixteenth
+            deriving (Show, Enum, Read, Bounded, Ord, Eq)
+
+data FullNote = FullNote Note Octave
+            deriving (Show, Read, Ord, Eq)
+
+type Scale = [FullNote]
+type Octave = Int
+type Measure = [(Beat, FullNote)]
+
 
 intervalBetween :: Note -> Note -> Interval
 intervalBetween n m = toBoundEnum (fromEnum m - fromEnum n)
@@ -16,10 +25,16 @@ bound n = n `mod` 12
 toBoundEnum :: (Enum a) => Int -> a
 toBoundEnum = toEnum.bound
 
-adjustNoteByStep :: Note -> Step -> Note
-adjustNoteByStep n s = toBoundEnum $ fromEnum n + fromEnum s
+toFullNote :: Int -> FullNote
+toFullNote n = FullNote (toBoundEnum n) (n `div` 12)
 
-createScale :: [Step] -> Note -> Scale
+adjustNoteByStep :: FullNote -> Step -> FullNote
+adjustNoteByStep n s = toFullNote $ fromFullNote n + fromEnum s
+
+fromFullNote :: FullNote -> Int
+fromFullNote (FullNote n m) = fromEnum n + m * 12
+
+createScale :: [Step] -> FullNote -> Scale
 createScale [] n = [n]
 createScale (step:steps) n = [n] ++ createScale steps (adjustNoteByStep n step)
 
