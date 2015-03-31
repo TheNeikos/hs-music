c9dfe1044fe7b093a8b73f97f9503c9c7dbf3a6d
Marcel Müller <neikos@neikos.email>
2015-03-31 02:24:43 +0200

    Added intervals

--- music.hs
+++ music.hs
@@ -2,11 +2,18 @@ data Note = C | C' | D | D' | E | F | F' | G | G'| A | A' | B
             deriving (Show, Enum, Read, Bounded, Ord, Eq)
 data Step = Base | Half | Whole deriving (Show, Enum, Read, Bounded, Ord, Eq)
 
+data Interval = P1 | Mi2 | M2 | Mi3 | M3 | P4 | P5 | Mi6 | M6 | Mi7 | M7 | P8
+            deriving (Show, Enum, Read, Bounded, Ord, Eq)
+
 type Scale = [Note]
+
+intervalBetween :: Note -> Note -> Interval
+intervalBetween n m = toBoundEnum (fromEnum m - fromEnum n)
 
 bound :: Int -> Int
 bound n = n `mod` 12
 
+toBoundEnum :: (Enum a) => Int -> a
 toBoundEnum = toEnum.bound
 
 adjustNoteByStep :: Note -> Step -> Note
@@ -18,3 +25,5 @@ createScale (step:steps) n = [n] ++ createScale steps (adjustNoteByStep n step)
 
 majorScale = createScale [Whole, Whole, Half, Whole, Whole, Whole]
 minorScale = createScale [Whole, Half, Whole, Whole, Half, Whole]
+
+
