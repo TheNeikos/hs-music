00cb704001384702adeee0d95e21f80c0b6fed04
Marcel Müller <neikos@neikos.email>
2015-03-31 17:24:46 +0200

    Added midi export

--- music.hs
+++ music.hs
@@ -1,3 +1,5 @@
+import Codec.Midi
+
 data Note = C | C' | D | D' | E | F | F' | G | G'| A | A' | B
             deriving (Show, Enum, Read, Bounded, Ord, Eq)
 data Step = Base | Half | Whole deriving (Show, Enum, Read, Bounded, Ord, Eq)
@@ -14,6 +16,7 @@ data FullNote = FullNote Note Octave
 type Scale = [FullNote]
 type Octave = Int
 type Measure = [(Beat, FullNote)]
+type Piece = [Measure]
 
 
 intervalBetween :: Note -> Note -> Interval
@@ -34,11 +37,26 @@ adjustNoteByStep n s = toFullNote $ fromFullNote n + fromEnum s
 fromFullNote :: FullNote -> Int
 fromFullNote (FullNote n m) = fromEnum n + m * 12
 
+fromBeat :: Beat -> Int
+fromBeat b = 24 `div` (2 ^ fromEnum b :: Int)
+
 createScale :: [Step] -> FullNote -> Scale
 createScale [] n = [n]
 createScale (step:steps) n = [n] ++ createScale steps (adjustNoteByStep n step)
 
-majorScale = createScale [Whole, Whole, Half, Whole, Whole, Whole]
-minorScale = createScale [Whole, Half, Whole, Whole, Half, Whole]
+majorScale = createScale [Whole, Whole, Half, Whole, Whole, Whole, Half]
+minorScale = createScale [Whole, Half, Whole, Whole, Half, Whole, Half]
 
+measureToTrack m = innerConvert m 0 where
+    innerConvert []     t = [(t, TrackEnd)]
+    innerConvert (x:xs) t = [(t, NoteOn 2 note 80)]
+                              ++ [(beat, NoteOff 2 note 0)]
+                              ++ innerConvert xs beat
+        where
+        beat = (fromBeat (fst x))
+        note = (fromFullNote (snd x) + 60)
 
+pieceToMidi m = Midi {
+            fileType = MultiTrack,
+            tracks = map measureToTrack m,
+            timeDiv = TicksPerBeat 24 }
